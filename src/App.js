5d946468673b39dbbddde57ff86df608634f7774
Menyus11 <senkialfonz11@gmail.com>
2022-09-29 19:21:52 +0200

    React learning

--- src/App.js
+++ src/App.js
@@ -3,43 +3,60 @@ import React from 'react';
 import './App.css';
 
 function ListItem(props) {
-   const [visible, setVisible] = useState(true);
+  const [visible, setVisible] = useState(false);
 
   return <React.Fragment>
-    {visible &&
-      <div >
-        <ul>
-          <li>username: {props.userItems.username}</li>
-          <li>email: {props.userItems.email}</li>
-          <li>phone: {props.userItems.phone}</li>
-          <li>website: {props.userItems.website}</li>
-          <li>company: {props.userItems.company.name}, motto: {props.userItems.company.catchPhrase}</li>
-          <li>address: {props.userItems.address.zipcode} {props.userItems.address.city} {props.userItems.address.street} {props.userItems.address.suite}</li>
-        </ul>
+    <div className='bg-warning rounded p-3 maindiv' onClick={() => { setVisible(true) }}>
+      <p>{props.idNow}</p>
+      <h3>{props.commentsItem.title}</h3>
+      <p>{props.commentsItem.body}</p>
+      {props.comments[0].filter(userItems => userItems.id === props.commentsItem.userId)
 
-        <p>comments:</p>
-        <ul>{props.comments[2].filter(postItem => props.idNow === postItem.postId)
-          .map((postItem, postIndex) => {
-            return <React.Fragment key={postIndex}>
-              <li>{postItem.body}</li>
-            </React.Fragment>
+        .map((userItems, userIndex) => {
 
-          })}</ul>
+          return <React.Fragment key={userIndex}>
+            <b className='postAuthor'>{userItems.name}</b>
+            <small>, posztjai száma: <b>{props.postAll.filter(post => post.userId === userItems.id).length}</b></small>
 
-        <button className='btn btn-danger form-control'
-          onClick={(e) => {
-            e.stopPropagation();
-            setVisible(false);
-          }}>
-          Összecsukás</button>
-      </div>}
+            {visible && (
 
+              <div className='mt-3'>
+                <ul>
+                  <li>username: {userItems.username}</li>
+                  <li>email: {userItems.email}</li>
+                  <li>phone: {userItems.phone}</li>
+                  <li>website: {userItems.website}</li>
+                  <li>company: {userItems.company.name}, motto: {userItems.company.catchPhrase}</li>
+                  <li>address: {userItems.address.zipcode} {userItems.address.city} {userItems.address.street} {userItems.address.suite}</li>
+                </ul>
+
+                <p>comments:</p>
+                <ul>{props.comments[2].filter(postItem => props.idNow === postItem.postId)
+                  .map((postItem, postIndex) => {
+
+                    return <React.Fragment key={postIndex}>
+                      <li>{postItem.body}</li>
+                    </React.Fragment>
+
+                  })}</ul>
+
+                <button className='btn btn-danger form-control'
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setVisible(false);
+                  }}>
+                  Összecsukás</button>
+              </div>
+            )}
+
+          </React.Fragment>
+        })}
+    </div>
+    <hr />
   </React.Fragment>
 }
 
-
-function App({setVisible}) {
-
+function App({ setVisible }) {
   const [comments, setComments] = useState([]);
 
   useEffect(() => {
@@ -66,53 +83,17 @@ function App({setVisible}) {
       <div className='container bg-success p-5 rounded'>
         {
           comments[1].map((commentsItem, commentIndex) => {
-            const idNow = commentsItem.id;
+
             return (
               <React.Fragment key={commentIndex}>
-                <div className='bg-warning rounded p-3 maindiv' onClick={() => { /* setVisible(true) */ }}>
-                  <p>{idNow}</p>
-                  <h3>{commentsItem.title}</h3>
-                  <p>{commentsItem.body}</p>
-                  {comments[0].filter(userItems => userItems.id === commentsItem.userId)
-
-                    .map((userItems, userIndex) => {
-
-                      return <React.Fragment key={userIndex}>
-                        <b className='postAuthor'>{userItems.name}</b>
-                        <small id={'small' + idNow}></small>
-
-                        <ListItem userItems={userItems} comments={comments} idNow={idNow}/>
-
-                      </React.Fragment>
-
-                    })}
-                </div>
-                <hr />
+                <ListItem postAll={comments[1]} comments={comments} idNow={commentsItem.id} commentsItem={commentsItem} />
               </React.Fragment>)
 
           })
         }
       </div>
     );
-
   }
-
-  /*     setTimeout(() => {
-        let usersPost = 0;
-        document.querySelectorAll('.postAuthor').forEach((element, index) => {
-          let postIdNow = index + 1;
-    
-          document.querySelectorAll('.postAuthor').forEach(e => {
-            if (e.innerText === element.innerText) {
-              usersPost++;
-              document.getElementById(`small` + postIdNow).innerHTML = (` (${usersPost} poszt)`);
-            }
-          })
-    
-          usersPost = 0;
-        })
-      }, 300); */
-
 }
 
 export default App;
