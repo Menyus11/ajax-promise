1bcfa0533d8eb52f595b44706aee7c2c8cf9e96b
Menyus11 <senkialfonz11@gmail.com>
2022-09-29 19:40:04 +0200

    react learning

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 import ListItem from './componens';
 
-function App({ setVisible }) {
+function App() {
   const [comments, setComments] = useState([]);
 
   useEffect(() => {
